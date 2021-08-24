807806317ddfcdcdb17178c2a4d4678a6f0c0004
hang_humg@outlook.com <hang_humg@outlook.com>
2021-08-24 16:24:35 +0700

    Add project files.

---
+++ Baitap2MVC/Controllers/GiaiPTBac1Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Baitap2MVC.Models;
+namespace Baitap2MVC.Controllers
+{
+    public class GiaiPTBac1Controller : Controller
+    {
+        GiaiPTBac1 gpt = new GiaiPTBac1();
+
+        // GET: GiaiPTBac1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GiaiPTBac1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GiaiPTBac1(double hesoa, double hesob)
+        {
+            double x = gpt.GiaiPTBacMot(hesoa, hesob);
+            ViewBag.nghiemPT = x;
+            return View();
+        }
+    }
+}
