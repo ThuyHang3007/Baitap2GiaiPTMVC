0ca8699af1f3c5e45881b7dd7587b4e7c6fe8f2d
hang_humg@outlook.com <hang_humg@outlook.com>
2021-08-27 14:13:02 +0700

    BaiTapThucHanhBuoi2

---
+++ Baitap2MVC/Controllers/GetinfoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baitap2MVC.Controllers
+{
+    public class GetinfoController : Controller
+    {
+        // GET: Getinfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Getinfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Getinfo(string Ten, string Tuoi, string Lop)
+        {
+
+            ViewBag.ten = Ten;
+            ViewBag.tuoi = Tuoi;
+            ViewBag.lop = Lop;
+
+            return View();
+        }
+    }
+}
